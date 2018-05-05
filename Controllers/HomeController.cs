c4f618d1690eec69158b0916370e787481f02ba3
qiangcheng@live.com <qiangcheng@live.com>
2018-05-05 13:10:31 +0800

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DivUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            HttpFileCollectionBase File = Request.Files;
+            string FileName = "/images/img/"+DateTime.Now.ToString("YYYYMMDDHHmmssfff");
+            if (File != null && File.Count > 0)
+            {
+                FileName += System.IO.Path.GetExtension(File[0].FileName);
+                File[0].SaveAs(Server.MapPath(FileName));
+            }
+            return Json(FileName);
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DivUpload.MvcApplication" Language="C#" %>
